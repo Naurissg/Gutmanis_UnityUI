5636bf163b44fd78b5bc53d1594252cfa8a90f96
Nauris <naurissgg@gmail.com>
2023-04-24 14:44:27 +0300

    Pievienota sakuma aina un opcija ievadit personas vardu un vecumu

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+	public GameObject JackSparrow;
+	public GameObject BusinessMan;
+	public GameObject Joe;
+	public GameObject krekls;
+	public GameObject Bikses;
+	public GameObject kurpes;
+	public GameObject cepure;
+	public GameObject paKreisi;
+	public GameObject paLabi;
+	public GameObject kurRadit;
+	public Sprite[] koRadit;
+	public GameObject slaideris1;
+	public GameObject slaideris2;
+
+	public void binaAttelosana(bool vertiba)
+	{
+		JackSparrow.SetActive(vertiba);
+		paKreisi.GetComponent<Toggle>().interactable = vertiba;
+		paLabi.GetComponent<Toggle>().interactable = vertiba;
+	}
+
+	public void KreklaAttelosana(bool vertiba)
+	{
+		krekls.SetActive(vertiba);
+	}
+
+	public void BiksuAttelosana(bool vertiba)
+	{
+		Bikses.SetActive(vertiba);
+	}
+
+	public void KurpesAttelosana(bool vertiba)
+	{
+		kurpes.SetActive(vertiba);
+	}
+	public void CepureAttelosana(bool vertiba)
+	{
+		cepure.SetActive(vertiba);
+	}
+	public void PaKreisiBins()
+	{
+		JackSparrow.transform.localScale = new Vector2(1, 1);
+	}
+
+	public void PaLabiBins()
+	{
+		JackSparrow.transform.localScale = new Vector2(-1, 1);
+	}
+
+	public void izkritosais(int indekss){
+		if (indekss == 0)
+			kurRadit.GetComponent<Image> ().sprite = koRadit [0];
+
+		else if(indekss == 1)
+			kurRadit.GetComponent<Image> ().sprite = koRadit [1];
+
+		else if(indekss == 2)
+			kurRadit.GetComponent<Image> ().sprite = koRadit [2];
+	}
+
+
+	public void mainitLielumu(){
+		float pasreizejaVertiba = slaideris1.GetComponent<Slider> ().value;
+		kurRadit.transform.localScale = 
+			new Vector2 (1f*pasreizejaVertiba, 1f*pasreizejaVertiba);
+	}
+
+	public void mainitRotaciju(){
+		float pasreizejaVertiba = slaideris2.GetComponent<Slider> ().value;
+		kurRadit.transform.localRotation =
+			Quaternion.Euler (0, 0, pasreizejaVertiba * 360);
+	}
+}
--- Assets/Skripti/VardaParadisana.cs
+++ Assets/Skripti/VardaParadisana.cs
@@ -1,43 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Nepieciešams, lai strādātu ar UI elementiem
 using UnityEngine.UI;
 
 public class VardaParadisana : MonoBehaviour {
-	/*int kadri = 0;
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log ("Programma tika startēta!");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log ("Kadrs: "+kadri);
-		kadri++;
-
-		if (kadri == 10) {
-			Debug.Log ("Tu sasniedzi 10. kadru!");
-		}
-	}*/
-
-	//mainīgais, kurā uzglabās tekstu no input field
 	private string teksts;
-	//lauks, kurā ievada tekstu
+	private int teksts1;
 	public GameObject ievadesLauks;
-	//text objekts, kurā attēlos tekstu pēc pogas nospiešanas
 	public GameObject tekstaAttelosana;
-	//masīvs ar iespējamajiem teksta sākumiem
+	public GameObject IevadesLauks2;
 	private string[] varianti = {"Sveiki ", "Labdien ", 
 		"Prieks Tevi redzēt ", "Uzredzēšanos "};
-	//mainīgais uzglabās random skaitli
 	int nejausais;
 
 	public void NolasitTekstu(){
 		teksts = ievadesLauks.GetComponent<Text>().text;
-		nejausais = Random.Range (0, varianti.Length);
-		tekstaAttelosana.GetComponent<Text>().text = 
-			varianti[nejausais]+teksts.ToUpper()+"!";
+		if (int.TryParse(IevadesLauks2.GetComponent<Text>().text, out teksts1)) {
+			nejausais = Random.Range(0, varianti.Length);
+			tekstaAttelosana.GetComponent<Text>().text = 
+				varianti[nejausais] + teksts.ToUpper() + " jus esat " + teksts1 + " gadus vecs!";
+		} else {
+			tekstaAttelosana.GetComponent<Text>().text = "Lūdzu, ievadiet skaitli savā vecumā!";
+		}
 	}		
 }
+
